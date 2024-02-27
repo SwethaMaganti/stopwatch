2857c124d13cbc7dfcf63a4a2f50c8b96170970b
swetha maganti <swethamaganti218@gmail.com>
2024-02-27 20:26:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,117 @@
+// let milliseconds=0 , seconds=0, minutes=0,hours=0;
+// let timeshow = document.getElementById("timeshow");
+// let timeref;
+ 
+
+// document.getElementById('start').onclick=function(){
+//   let  timeref = setInterval(updatetime,10)
+// }
+
+// function updatetime(){
+//     milliseconds=milliseconds+10;
+//     if(milliseconds==1000){
+//         seconds=seconds+1;
+//         milliseconds=0;
+//         if(seconds==60){
+//             minutes=minutes+1;
+//             seconds=0;
+//             if(minutes==60){
+//                 hours=hours+1;
+//                 minutes=0;
+//             }
+//         }
+//     }
+// }
+
+
+// let h,m,s,ms;
+// if(hours<10){
+//     h="0"+hours;
+// }
+// else{
+//     h=hours;
+// }
+// if(minutes<10){
+//     m="0"+minutes;
+// }
+// else{
+//     m=minutes;
+// }
+// if(seconds<10){
+//     s="0"+seconds;
+// }
+// else{
+//     s=seconds;
+// }
+
+// if(milliseconds<10){
+//     ms="0"+milliseconds;
+// }
+// else if(milliseconds<100){
+//     ms="00"+milliseconds;
+// }
+// else{
+//     ms=milliseconds
+// }
+// timeshow.innerHTML = '${h} : ${m} :${s} : ${ms}'
+
+
+// document.getElementById("pause").onclick = function(){
+//     clearInterval("timeref")
+// }
+// document.getElementById("pause").onclick = function(){
+//     clearInterval("timeref")
+//     milliseconds=0;
+//     seconds=0;
+//     minutes=0;
+//     hours=0;
+//     timeshow.innerHTML="00 : 00 : 00 : 00"
+
+// }
+
+let milliseconds = 0, seconds = 0, minutes = 0, hours = 0;
+let timeshow = document.getElementById("timeshow");
+let timeref;
+
+document.getElementById('start').onclick = function() {
+    clearInterval(timeref);
+    timeref = setInterval(updatetime, 10);
+}
+
+document.getElementById("pause").onclick = function() {
+    clearInterval(timeref);
+}
+
+document.getElementById("reset").onclick = function() {
+    clearInterval(timeref);
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    updateDisplay();
+}
+
+function updatetime() {
+    milliseconds += 10;
+    if (milliseconds == 1000) {
+        seconds++;
+        milliseconds = 0;
+        if (seconds == 60) {
+            minutes++;
+            seconds = 0;
+            if (minutes == 60) {
+                hours++;
+                minutes = 0;
+            }
+        }
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    let h = (hours < 10) ? "0" + hours : hours;
+    let m = (minutes < 10) ? "0" + minutes : minutes;
+    let s = (seconds < 10) ? "0" + seconds : seconds;
+    let ms = (milliseconds < 10) ? "00" + milliseconds : (milliseconds < 100) ? "0" + milliseconds : milliseconds;
+    timeshow.innerHTML = h + ' : ' + m + ' : ' + s + ' : ' + ms;
+}
